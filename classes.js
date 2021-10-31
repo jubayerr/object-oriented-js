746b132a212f9666cde998abb252c1529f6eed55
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-31 11:10:55 +0600

    classes

---
+++ classes.js
@@ -0,0 +1,16 @@
+// class
+
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+}
+
+let myCar1 = new Car("Ford", 2014);
+let myCar2 = new Car("Audi", 2019);
+
+console.log(myCar1);
+console.log(myCar2);
+
+// ... end
